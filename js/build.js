975379fae496f104f61d7cd01690fbdd84309e1d
Exie <30753109+jxbc@users.noreply.github.com>
2024-04-22 22:30:11 +0300

    v1.2.1

--- js/build.js
+++ js/build.js
@@ -1,5 +1,62 @@
+let rox = new Rox
+
 async function main() {
-	let rox = new Rox
+	parseCode()
+}
+
+async function parseCode() {
+	let codes = document.querySelectorAll('#c0deExample')
+	for(let cd in codes) {
+		if(cd == 0) {
+			let code = `const rox = new <span class="yellow">Rox</span>
+			const <span class="green">Router</span> = rox.router() <span class="gray noselect">// инициализация роутинга</span>
+			
+			let button = <span class="yellow">document</span>.querySelector('button')
+			let contentBlock = <span class="yellow">document</span>.querySelector('.content')
+			button.addEventListener('click', async () => {
+			\tab <span class="green">Router</span>.go('friends', contentBlock, '.content')
+			})`
+
+			let syntax = rox.codeSyntax(code)
+			codes[cd].innerHTML = syntax
+		}
+		if(cd == 1) {
+			let code = `let Header, Footer, Content, Post, NotFound
+			(async () => {
+				\tab Header = await import('components/header.js') 
+				\tab Footer = await import('components/footer.js') 
+				\tab MusicContent = await import('components/music') 
+				\tab Post = await import('components/post') 
+				\tab NotFound = await import('components/notfound') 
+			})()
+			const paths = [{in: '/music', to: MusicContent}, {in: '/post/$ID', to: Post}, {in: 404, to: NotFound}] 
+			
+			const rox = new <span class="yellow">Rox</span>
+			const <span class="green">Router</span> = rox.virtualRouter(paths) <span class="gray noselect">// инициализация роутинга</span>
+			
+
+			function MusicButton() {
+				\tab let button = <span class="yellow">document</span>.querySelector('button')
+				\tab let root = <span class="yellow">document</span>.querySelector('body')
+					\tab <span class="green">Router</span>.go('/music', <span class="gray">{click: button, target: root}</span>, <span class="lime">(next)</span> => {
+				\tab\tab next()
+			\tab })
+			}
+			`
+
+			let syntax = rox.codeSyntax(code)
+			codes[cd].innerHTML = syntax
+		}
+		if(cd == 2) {
+			let code = `<span class="green">Router</span>.go('/music', <span class="gray">{click: button, target: root}</span>, <span class="lime">(next)</span> => {
+				\tab next()
+			})
+			`
+
+			let syntax = rox.codeSyntax(code)
+			codes[cd].innerHTML = syntax
+		}
+	}
 }
 
 main()
--- js/rox.js
+++ js/rox.js
@@ -6,4 +6,44 @@ class Rox {
 			}
 		}
 	}
+
+	codeSyntax(input) {
+		input = String(input)
+		input = input.replaceAll('\n', '<br>')
+		input = input.replaceAll('\tab', '<span class="tab noselect"></span>')
+		let keys = {
+			'let': '<span class="magenta">let</span>',
+			'var': '<span class="magenta">let</span>',
+			'null': '<span class="orange">null</span>',
+			'if': '<span class="magenta">if</span>',
+			'else': '<span class="magenta">else</span>',
+			'const': '<span class="blue">const</span>',
+			'function': '<span class="magenta">function</span>',
+			'return': '<span class="magenta">return</span>',
+			'async': '<span class="purple">async</span>',
+			'await': '<span class="purple">await</span>',
+			'for': '<span class="purple">for</span>',
+			'while': '<span class="purple">while</span>',
+			'true': '<span class="blue">true</span>',
+			'false': '<span class="orange">false</span>',
+			'new': '<span class="lime">new</span>',
+			'String': '<span class="yellow">String</span>',
+			'Object': '<span class="yellow">Object</span>',
+			'console': '<span class="red">console</span>',
+			'print': '<span class="blue">print</span>',
+			'in': '<span class="blue">in</span>',
+			'of': '<span class="blue">of</span>',
+			'def': '<span class="green">def</span>',
+			'import': '<span class="red">import</span>',
+			'from': '<span class="red">from</span>',
+			'default': '<span class="red">default</span>',
+			'export': '<span class="red">export</span>',
+			'=>': '<span class="lime">=></span>'
+		}
+
+		let regex = new RegExp(Object.keys(keys).join('|'), 'g');
+		return input.replace(regex, function(match) {
+	        return keys[match];
+	    });
+	}
 }
